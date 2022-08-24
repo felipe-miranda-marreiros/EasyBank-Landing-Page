3b610ac8610b4ee4bcea59504c01d9507352bf4a
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-08-23 23:31:48 -0300

    feat: add mobile menu links on click

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,22 @@
 const btnHamburger = document.getElementById('button-hamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', () => {
   if (header.classList.contains('open')) {
+    document.body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElems.forEach((el) => {
+      el.classList.remove('fade-in');
+      el.classList.add('fade-out');
+    });
   } else {
+    document.body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    fadeElems.forEach((el) => {
+      el.classList.remove('fade-out');
+      el.classList.add('fade-in');
+    });
   }
 });
